10d278cafd2017753bfbc720ffccc23cc1efa482
Philip Chang <sgnoohc@gmail.com>
2019-04-18 12:19:51 -0700

    Added a script to check git hashes

---
+++ check_git_hash.sh
@@ -0,0 +1,30 @@
+echo analysis/rooutil
+cd analysis/rooutil
+git rev-parse HEAD
+cd -
+echo analysis/plottery
+cd analysis/plottery
+git rev-parse HEAD
+cd -
+echo fakerate/plottery
+cd fakerate/plottery
+git rev-parse HEAD
+cd -
+echo fakerate/rooutil
+cd fakerate/rooutil
+git rev-parse HEAD
+cd -
+echo fakerate/ProjectMetis
+cd fakerate/ProjectMetis
+git rev-parse HEAD
+cd -
+echo analysis/ProjectMetis
+cd analysis/ProjectMetis
+git rev-parse HEAD
+cd -
+echo wwwbabymaker
+cd wwwbabymaker
+git rev-parse HEAD
+echo sh check_git_hash.sh
+sh check_git_hash.sh
+cd -
