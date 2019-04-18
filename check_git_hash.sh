bc45d9f43f8f3397d6442abcfe174845dbcee381
Philip Chang <sgnoohc@gmail.com>
2019-04-18 12:41:42 -0700

    update

--- check_git_hash.sh
+++ check_git_hash.sh
@@ -1,30 +1,38 @@
+git submodule update --recursive
 echo analysis/rooutil
 cd analysis/rooutil
+git status | head -n 1
 git rev-parse HEAD
-cd -
+cd - > /dev/null
 echo analysis/plottery
 cd analysis/plottery
+git status | head -n 1
 git rev-parse HEAD
-cd -
+cd - > /dev/null
 echo fakerate/plottery
 cd fakerate/plottery
+git status | head -n 1
 git rev-parse HEAD
-cd -
+cd - > /dev/null
 echo fakerate/rooutil
 cd fakerate/rooutil
+git status | head -n 1
 git rev-parse HEAD
-cd -
+cd - > /dev/null
 echo fakerate/ProjectMetis
 cd fakerate/ProjectMetis
+git status | head -n 1
 git rev-parse HEAD
-cd -
+cd - > /dev/null
 echo analysis/ProjectMetis
 cd analysis/ProjectMetis
+git status | head -n 1
 git rev-parse HEAD
-cd -
+cd - > /dev/null
 echo wwwbabymaker
 cd wwwbabymaker
+git status | head -n 1
 git rev-parse HEAD
 echo sh check_git_hash.sh
 sh check_git_hash.sh
-cd -
+cd - > /dev/null
